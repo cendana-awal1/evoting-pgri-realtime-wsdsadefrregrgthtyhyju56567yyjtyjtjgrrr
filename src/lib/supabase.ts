060d2f4e5a96bf142e2a574d8b0d57aea9a7adad
awal-cendana <108106499+cendana-awal1@users.noreply.github.com>
2025-01-10 11:28:03 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,7 +4,7 @@ import type { Database } from './database.types';
 export const supabase = createClient<Database>(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6c3JhcHljeWFsYm9yeGZvaGd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYwODg5MTMsImV4cCI6MjA1MTY2NDkxM30.RZsLOvDu0HOrApuV16wvGXpa_6LyKDc9qfbMm1lSkSs'
 ,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'https://szsrapycyalborxfohgt.supabase.co'
 );
 
 export type Candidate = Database['public']['Tables']['candidates']['Row'];
